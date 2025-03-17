a239bdfc8dff7b65a27a4a9eff3745d1b8301901
Tanmaymaity17 <tanmay004maity@gmail.com>
2025-03-17 15:27:52 +0530

    initial commit and basic onboarding screens added

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignIn = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-general-400">SignIn</Text>
+        </SafeAreaView>
+    );
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUP = () => {
+    return (
+        <SafeAreaView>
+            <Text className="text-general-400">SignUP</Text>
+        </SafeAreaView>
+    );
+};
+
+export default SignUP;
